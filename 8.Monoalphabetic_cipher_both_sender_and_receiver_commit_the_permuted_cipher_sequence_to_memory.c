6bb98d440eda7213ee47b70a696eda2613f8c9f7
AKHIL KUMAR REDDY <114383206+Akhil9392@users.noreply.github.com>
2026-07-06 10:18:10 +0530

    Update 8.Monoalphabetic_cipher_both_sender_and_receiver_commit_the_permuted_cipher_sequence_to_memory.c

--- 8.Monoalphabetic_cipher_both_sender_and_receiver_commit_the_permuted_cipher_sequence_to_memory.c
+++ 8.Monoalphabetic_cipher_both_sender_and_receiver_commit_the_permuted_cipher_sequence_to_memory.c
@@ -2,15 +2,12 @@
 #include <string.h>
 #include <ctype.h>
 
-// Function to generate the cipher sequence from the keyword
 void generateCipherSequence(char keyword[], char cipherSequence[]) {
     int keywordLength = strlen(keyword);
     int index = 0;
 
-    // Copy the keyword to the cipher sequence
     strcpy(cipherSequence, keyword);
 
-    // Append the unused letters in normal order
     for (char ch = 'A'; ch <= 'Z'; ++ch) {
         if (strchr(keyword, toupper(ch)) == NULL) {
             cipherSequence[keywordLength + index] = ch;
@@ -18,17 +15,14 @@ void generateCipherSequence(char keyword[], char cipherSequence[]) {
         }
     }
 
-    // Null-terminate the cipher sequence
     cipherSequence[keywordLength + index] = '\0';
 }
 
-// Function to perform monoalphabetic substitution cipher encryption
 void monoalphabeticCipher(char message[], char cipherSequence[]) {
     for (int i = 0; message[i] != '\0'; ++i) {
         if (isalpha(message[i])) {
             char base = isupper(message[i]) ? 'A' : 'a';
 
-            // Use the cipher sequence for substitution
             message[i] = cipherSequence[message[i] - base];
         }
     }
@@ -39,17 +33,13 @@ int main() {
     char cipherSequence[26];
     char message[100];
 
-    // Generate the cipher sequence from the keyword
     generateCipherSequence(keyword, cipherSequence);
 
-    // Input the message to be encrypted
     printf("Enter a message: ");
     fgets(message, sizeof(message), stdin);
 
-    // Perform monoalphabetic substitution cipher encryption
     monoalphabeticCipher(message, cipherSequence);
 
-    // Print the encrypted message
     printf("Encrypted message: %s", message);
 
     return 0;
